4b6de2b250effde519580ad1cef4ef11c38688ad
shurik204 <iwmdiif@gmail.com>
2024-01-09 21:24:40 +0200

    Add flux device data when picking a block

--- src/main/java/sonar/fluxnetworks/register/ClientRegistration.java
+++ src/main/java/sonar/fluxnetworks/register/ClientRegistration.java
@@ -1,14 +1,19 @@
 package sonar.fluxnetworks.register;
 
 import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.event.client.player.ClientPickBlockGatherCallback;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.BlockHitResult;
 import sonar.fluxnetworks.FluxNetworks;
+import sonar.fluxnetworks.api.FluxConstants;
 import sonar.fluxnetworks.client.FluxColorHandler;
 import sonar.fluxnetworks.client.gui.GuiFluxAdminHome;
 import sonar.fluxnetworks.client.gui.GuiFluxDeviceHome;
@@ -32,6 +37,25 @@ public class ClientRegistration {
         ClientPlayNetworking.registerGlobalReceiver(Channel.CHANNEL_NAME, (client, handler, buf, responseSender) ->
                 ClientMessages.msg(buf.readShort(), buf, () -> Minecraft.getInstance().player));
         // onPlayerLoggedOut is now handled by MinecraftClientMixin
+        // Add device data to the picked block
+        ClientPickBlockGatherCallback.EVENT.register((player, result) -> {
+            // TODO: fix
+            if (result instanceof BlockHitResult blockHit && player.level().getBlockEntity(blockHit.getBlockPos()) instanceof TileFluxDevice fluxDevice) {
+                ItemStack stack = fluxDevice.getDisplayStack();
+                // Fix colors on storages
+                stack.removeTagKey(FluxConstants.FLUX_COLOR);
+                // Copy data from the TileEntity
+                CompoundTag tag = stack.getOrCreateTagElement(FluxConstants.TAG_FLUX_DATA);
+                tag.putInt(FluxConstants.NETWORK_ID, fluxDevice.getNetworkID());
+                tag.putString(FluxConstants.CUSTOM_NAME, fluxDevice.getCustomName());
+                tag.putLong(FluxConstants.LIMIT, fluxDevice.getRawLimit());
+                tag.putLong(FluxConstants.PRIORITY, fluxDevice.getRawPriority());
+
+                return stack;
+            }
+
+            return ItemStack.EMPTY;
+        });
     }
 
     @Nonnull
